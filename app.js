de6ba652265157b49240c02a1245e1cc7ae6a30a
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-06-04 19:05:48 -0300

    Compress image

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 const sharp = require("sharp");
+const compress_images = require("compress-images");
+const fs = require("fs");
 
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 
-sharp(path)
-  .resize(width)
-  .toFile('./temp/output.jpg', (error) => {
+function resize(inputPath, outputPath, width) {
+  sharp(inputPath)
+    .rotate(45)
+    .resize(width)
+    .toFile(outputPath, (error) => {
+      if (error) {
+        console.log(`Erro ao redimensionar a imagem -> ${error.message}`);
+      } else {
+        console.log('Imagem redimensionada com sucesso');
+        compress(outputPath, './compressed/');
+      }
+    });
+}
+
+function compress(inputPath, outputPath) {
+  compress_images(inputPath, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+    function (error, completed, statistic) {
+      console.log("-------------");
+      console.log(error);
+      console.log(completed);
+      console.log(statistic);
+      console.log("-------------");
+
+      deleteFileTemp(inputPath);
+    }
+  );
+}
+
+function deleteFileTemp(inputFile) {
+  fs.unlink(inputFile, (error) => {
     if (error) {
-      console.log(`Erro ao redimensionar a imagem -> ${error.message}`);
+      console.log(`Error ao excluir o arquivo temporário -> ${error.code}: ${error.message}`);
     } else {
-      console.log('Imagem redimensionada com sucesso');
+      console.log('Arquivo excluido com sucesso');
     }
   });
+}
+
+resize(path, './temp/output.jpg', width);
