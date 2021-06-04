b11760c0df6fd659028c20f956de3e5a715ef494
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-06-04 19:56:40 -0300

    Read pages file

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
 http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type': 'text/html' });
-  response.end('<h1>Hello Word!</h2>');
+
+  let path = url.parse(request.url).pathname;
+
+  if (path == "" || path == "/") {
+    path = "/index.html";
+  }
+
+  let fileName = `.${path}`;
+
+  fs.readFile(fileName, (error, data) => {
+    if (error) {
+      response.writeHead(404, { 'Content-type': 'text/html;charset=utf-8' });
+      response.end('<h1>Página não encontrada</h1>');
+    } else {
+      response.writeHead(200, { 'Content-type': 'text/html' });
+      response.write(data);
+      response.end();
+    }
+  });
+
 }).listen(3000, (error) => {
   if (error) {
     console.log(`Erro ao criar o servidor ${error}`);
