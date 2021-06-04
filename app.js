1b1e9046fdd7afeb349487bd7b230da64f86894a
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-06-04 18:07:46 -0300

    Resize image

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const sharp = require("sharp");
+
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 
-console.log(path, width);
+sharp(path)
+  .resize(width)
+  .toFile('./temp/output.jpg', (error) => {
+    if (error) {
+      console.log(`Erro ao redimensionar a imagem -> ${error.message}`);
+    } else {
+      console.log('Imagem redimensionada com sucesso');
+    }
+  });
